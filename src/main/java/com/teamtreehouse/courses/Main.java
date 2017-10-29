cf785858e7e5ddad1a502083a3b4f8b6908a1c78
valencra <valencra@mcmaster.ca>
2017-10-29 19:00:00 -0400

    Implemented flash messages with sessions

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -5,6 +5,7 @@ import com.teamtreehouse.courses.model.CourseIdeaDAO;
 import com.teamtreehouse.courses.model.NotFoundException;
 import com.teamtreehouse.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
@@ -13,6 +14,8 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class Main {
+    private static final String FLASH_MESSAGE_KEY = "flash_message";
+
     public static void main(String[] args) {
         staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
@@ -24,8 +27,8 @@ public class Main {
         });
 
         before("/ideas", (req, res) -> {
-            // TODO:rv - Send message about redirect...somehow.
             if (req.attribute("username") == null) {
+                setFlashMessage(req, "Whoops, please sign in first");
                 res.redirect("/");
                 halt();
             }
@@ -34,6 +37,7 @@ public class Main {
         get("/", (req, res) -> {
                 Map<String, String> model = new HashMap<>();
                 model.put("username", req.attribute("username"));
+                model.put("flashMessage", captureFlashMessage(req));
                 return new ModelAndView(model, "index.hbs");
             }, new HandlebarsTemplateEngine());
 
@@ -49,6 +53,7 @@ public class Main {
         get("/ideas", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("ideas", dao.findAll());
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -68,7 +73,13 @@ public class Main {
 
         post("/ideas/:slug/vote", (req, res) -> {
             CourseIdea idea = dao.findBySlug(req.params("slug"));
-            idea.addVoter(req.attribute("username"));
+            boolean added = idea.addVoter(req.attribute("username"));
+            if (added) {
+                setFlashMessage(req, "Thanks for your vote!");
+            }
+            else {
+                setFlashMessage(req, "You already voted!");
+            }
             res.redirect("/ideas");
             return null;
         });
@@ -81,4 +92,27 @@ public class Main {
             res.body(html);
         });
     }
+
+    private static void setFlashMessage(Request req, String message) {
+        req.session().attribute(FLASH_MESSAGE_KEY, message);
+    }
+
+    private static String getFlashMessage(Request req) {
+        if (req.session(false) == null) {
+            return null;
+        }
+        if (!req.session().attributes().contains(FLASH_MESSAGE_KEY)) {
+            return null;
+        }
+        return (String) req.session().attribute(FLASH_MESSAGE_KEY);
+    }
+
+    private static String captureFlashMessage(Request req) {
+        String message = getFlashMessage(req);
+        if (message!=null) {
+            req.session().removeAttribute(FLASH_MESSAGE_KEY);
+        }
+        return message;
+    }
+
 }
