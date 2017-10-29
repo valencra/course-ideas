3192a5bb2b40b2109e25e322a95eb39e29c20dfe
valencra <valencra@mcmaster.ca>
2017-10-29 18:03:53 -0400

    Added voting mechanism

--- src/main/java/com/teamtreehouse/courses/model/CourseIdea.java
+++ src/main/java/com/teamtreehouse/courses/model/CourseIdea.java
@@ -1,12 +1,27 @@
 package com.teamtreehouse.courses.model;
 
+import com.github.slugify.Slugify;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CourseIdea {
     private String title;
     private String creator;
+    private String slug;
+    private Set<String> voters;
 
     public CourseIdea(String title, String creator) {
         this.title = title;
         this.creator = creator;
+        try {
+            Slugify slugify = new Slugify();
+            slug = slugify.slugify(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        voters = new HashSet<>();
     }
 
     public String getTitle() {
@@ -15,6 +30,18 @@ public class CourseIdea {
 
     public String getCreator() {
         return creator;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public boolean addVoter(String voterUserName) {
+        return voters.add(voterUserName);
+    }
+
+    public int getVoteCount() {
+        return voters.size();
     }
 
     @Override
